13d22672a19f2c1b1bef890b2f4babfc180faa5b
rusrom <19729227+rusrom@users.noreply.github.com>
2025-07-08 17:51:16 +0300

    self : add bloc to details view

--- lib/features/details/view/coin_details_screen.dart
+++ lib/features/details/view/coin_details_screen.dart
@@ -1,8 +1,11 @@
 // import 'dart:developer';
 
+import 'package:coinlist/features/details/bloc/details_bloc.dart';
 import 'package:coinlist/features/details/details.dart';
 import 'package:coinlist/repositories/crypto_api/api_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class CoinDetailsScreen extends StatefulWidget {
   const CoinDetailsScreen({super.key});
@@ -13,6 +16,8 @@ class CoinDetailsScreen extends StatefulWidget {
 
 class _CoinDetailsScreenState extends State<CoinDetailsScreen> {
   CryptoCoin? coin;
+
+  final _detailsBloc = DetailsBloc(GetIt.I<AbstractApiRepository>());
 
   @override
   void didChangeDependencies() {
@@ -33,7 +38,7 @@ class _CoinDetailsScreenState extends State<CoinDetailsScreen> {
     // }
 
     coin = args as CryptoCoin;
-    setState(() {});
+    _detailsBloc.add(LoadDetails(coinCode: coin!.name));
 
     super.didChangeDependencies();
   }
@@ -43,36 +48,50 @@ class _CoinDetailsScreenState extends State<CoinDetailsScreen> {
     return Scaffold(
       // appBar: AppBar(title: Text(coin!.name)),
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              width: 160,
-              height: 160,
-              child: Image.network(coin!.coinImageUrl),
-            ),
-            const SizedBox(height: 24),
-            Text(
-              coin!.name,
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(height: 18),
-            BaseCard(
-              child: Center(
-                child: Text(
-                  '${coin!.priceInUSD.toStringAsFixed(2)} \$'
-                ),
-              ),
-            ),
-            BaseCard(child: Column(
-              children: [
-                  DetailsDataRow(title: 'High 24 Hour', value:'3456343456 \$'),
-                  DetailsDataRow(title: 'Low 24 Hour', value:'67345673 \$'),
+      body: BlocBuilder<DetailsBloc, DetailsState>(
+        bloc: _detailsBloc,
+        builder: (context, state) {
+          if (state is DetailsLoaded) {
+            final coinDetails = state.coinDetails;
+            return Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 160,
+                    height: 160,
+                    child: Image.network(coin!.coinImageUrl),
+                  ),
+                  const SizedBox(height: 24),
+                  Text(
+                    coin!.name,
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(height: 18),
+                  BaseCard(
+                    child: Center(
+                      child: Text('${coin!.priceInUSD.toStringAsFixed(2)} \$'),
+                    ),
+                  ),
+                  BaseCard(
+                    child: Column(
+                      children: [
+                        DetailsDataRow(
+                          title: 'High 24 Hour',
+                          value: '${coinDetails.hight24Hour.toStringAsFixed(2)} \$',
+                        ),
+                        DetailsDataRow(
+                          title: 'Low 24 Hour',
+                          value: '${coinDetails.low24Hours.toStringAsFixed(2)} \$',
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
-        ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
